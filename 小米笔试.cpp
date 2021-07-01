2179471ccd2b9e4f66a4fb5c21ecb3ab40f4a955
Garrett <13546043646@163.com>
2021-07-01 18:54:51 +0800

    2021年7月1日18:54:50

--- 小米笔试.cpp
+++ 小米笔试.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<functional>
 #include<stack>
 #include<vector>
 #include<algorithm>
@@ -58,35 +59,51 @@ int main2() {
 /*
 	平分物品
 */
-
-void check(vector<int>& vec) {
-	sort(vec.begin(), vec.end(), [](const auto& num1, const auto& num2) {return num1 > num2; });
-	int len = vec.size();
-	int AValue, BValue;
-	AValue = vec[0]; BValue = 0;
-	while (1) {
-		for (int i = 1; i < len; ++i) {
-			if (accumulate(vec.begin() + i, vec.end(), BValue) > AValue) {
-
-			}
-		}
-	}
-}
-
-int main() {
+int main3() {
 	int T;
 	cin >> T;
-	vector<int> vec;
 	for (int i = 0; i < T; ++i) {
 		int num;
 		cin >> num;
+		vector<int> vec;
 		for (int j = 0; j < num; ++j) {
 			int tmp;
 			cin >> tmp;
 			vec.push_back(tmp);
 		}
-		check(vec);
-		return 0;
+		int result = 0;
+		int totalValue = accumulate(vec.begin(), vec.end(), 0);
+		function<void(int, int, int)> dfs = [&](int index, int val1, int val2) {
+			if (val1 == val2) {
+				result = max(result, val1 + val2);
+			}
+			if (index == num || val1 > totalValue / 2 || val2 > totalValue / 2) {
+				return;
+			}
+			dfs(index + 1, val1 + vec[index], val2);
+			dfs(index + 1, val1, val2 + vec[index]);
+			dfs(index + 1, val1, val2);
+		};
+		dfs(0, 0, 0);
+		cout << totalValue - result << endl;
 	}
+	return 0;
+}
+
+/*
+	最短回文串
+*/
+int main() {
+	string str;
+	cin >> str;
+	int len = str.size();
+	for (int i = 0; i < len; ++i) {
+		string temp;
+		temp = str;
+		reverse(temp.begin(), temp.end());
+		if (str == temp) break;
+		str.insert(str.begin() + len, str[i]);
+	}
+	cout << str << endl;
 	return 0;
 }
